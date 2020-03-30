91dd90bc1833af9571b9d59d1c8fb52d47fae914
Macário Martins <macariomartinsjunior@gmail.com>
2020-03-30 20:18:03 -0300

    Correção de porta para compatibilidade com ambiente de desevolvimento local

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3333;
 
 app.listen(port, () => {
     console.log('This app is listening port ' + port);
